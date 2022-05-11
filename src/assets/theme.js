369f57022dc50edd1e307496109a5b839f54aa28
Mel Incognito <melxincognito@gmail.com>
2022-05-11 10:39:11 -0700

    created theme

---
+++ src/assets/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#212121",
+    },
+    secondary: {
+      main: "#6a1b9a",
+    },
+  },
+  typography: {
+    fontFamily: "Libre Bodoni",
+  },
+});
+
+export default theme;
